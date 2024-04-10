564f02969ab7b101d5365b30a11ce53f6e4e9926
Jubair <jubairislam1010@gmail.com>
2024-04-10 15:43:33 +0600

    HR and Accountant

---
+++ Dhaka MetroRail Final/src/view/jubair/accountant/AccountantDashController.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package view.jubair.accountant;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author user
+ */
+public class AccountantDashController implements Initializable {
+
+    @FXML
+    private Button dailyTransactionButton;
+    @FXML
+    private Button employeePayroll;
+    @FXML
+    private Button vendorManage;
+    @FXML
+    private Button insurance;
+    @FXML
+    private Button discountCoupon;
+    @FXML
+    private Button transactionManagement;
+    @FXML
+    private Button backAccountant;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void dailyTransactionOnMouseClick(MouseEvent event) {
+    }
+
+    @FXML
+    private void employeePayrollOnMouseClick(MouseEvent event) {
+    }
+
+    @FXML
+    private void vendorManageOnMouseClick(MouseEvent event) {
+    }
+
+    @FXML
+    private void inventoryAssetOnMouseClick(MouseEvent event) {
+    }
+
+    @FXML
+    private void inventoryAsset(ActionEvent event) {
+    }
+
+    @FXML
+    private void insuranceOnMouseClick(MouseEvent event) {
+    }
+
+    @FXML
+    private void discountCouponOnMouseClick(MouseEvent event) {
+    }
+
+    @FXML
+    private void transactionManagementOnMouseClick(MouseEvent event) {
+    }
+
+    @FXML
+    private void backAccountantOnMouseClick(MouseEvent event) {
+    }
+    
+}
