51ba3765a299a0857224604599daa37e403a59d5
Faysal Kabir Ashik <faysalkabirashik@gmail.com>
2024-04-12 22:58:09 +0600

    update  kore na ken

---
+++ Dhaka MetroRail Final/src/model/faysal/users/LoginInfo.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.faysal.users;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Faysal Kabir Ashik
+ */
+public class LoginInfo implements Serializable {
+    
+    private String userIdentity;
+    String userType;
+    private String password;
+
+    public String getUserIdentity() {
+        return userIdentity;
+    }
+
+    public void setUserIdentity(String userIdentity) {
+        this.userIdentity = userIdentity;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
