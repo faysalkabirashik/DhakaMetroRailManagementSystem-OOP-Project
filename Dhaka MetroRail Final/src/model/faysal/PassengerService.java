5f824492cd5b0d29d2ecb54424d86c1f847c0fce
Faysal Kabir Ashik <faysalkabirashik@gmail.com>
2024-04-14 03:17:36 +0600

    model classes

---
+++ Dhaka MetroRail Final/src/model/faysal/PassengerService.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.faysal;
+
+/**
+ *
+ * @author Faysal Kabir Ashik
+ */
+public class PassengerService {
+    
+      private String serviceType;
+    private  String inquiry;
+    private String assistance;
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public String getInquiry() {
+        return inquiry;
+    }
+
+    public void setInquiry(String inquiry) {
+        this.inquiry = inquiry;
+    }
+
+    public String getAssistance() {
+        return assistance;
+    }
+
+    public void setAssistance(String assistance) {
+        this.assistance = assistance;
+    }
+    
+    
+    
+    
+}
---
+++ Dhaka MetroRail Final/src/model/faysal/TrainSchedule.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.faysal;
+
+import java.util.List;
+
+/**
+ *
+ * @author Faysal Kabir Ashik
+ */
+public class TrainSchedule {
+    
+     
+    private Train train;
+    private Station station;
+    
+    private String arrivalTieme;
+    private double stayTime;
+
+    public TrainSchedule(Train train, Station station, String arrivalTieme, double stayTime) {
+        this.train = train;
+        this.station = station;
+        this.arrivalTieme = arrivalTieme;
+        this.stayTime = stayTime;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
+    public Station getStation() {
+        return station;
+    }
+
+    public void setStation(Station station) {
+        this.station = station;
+    }
+
+    public String getArrivalTieme() {
+        return arrivalTieme;
+    }
+
+    public void setArrivalTieme(String arrivalTieme) {
+        this.arrivalTieme = arrivalTieme;
+    }
+
+    public double getStayTime() {
+        return stayTime;
+    }
+
+    public void setStayTime(double stayTime) {
+        this.stayTime = stayTime;
+    }
+
+ 
+ 
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("TrainSchedule{");
+        sb.append("train=").append(train);
+        sb.append(", station=").append(station);
+        sb.append(", arrivalTieme=").append(arrivalTieme);
+        sb.append(", stayTime=").append(stayTime);
+  
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+}
